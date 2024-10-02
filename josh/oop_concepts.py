8f4802d9af3e8de7159addc31a84ebfcdda7c3a3
zeynalhajili <hacizeynal@hotmail.com>
2024-10-03 00:09:08 +0200

    update

--- josh/oop_concepts.py
+++ josh/oop_concepts.py
@@ -8,25 +8,46 @@
 # red_turtle=Turtle()
 # red_turtle.color("red")
 
-class Animal():
-    def __init__(self, name, pet, sound):
-        self.name =name
-        self.pet =pet
-        self.sound =sound
+# class Animal():
+#     def __init__(self, name, pet, sound):
+#         self.name =name
+#         self.pet =pet
+#         self.sound =sound
         
-    def speak(self):
-        print(self.sound)
+#     def speak(self):
+#         print(self.sound)
     
-    def pet_info(self):
-        print("My",self.pet,"has the name",self.name,"they make sound",self.sound)
-    
-
-dog =Animal("XIRT","IT","HAVHAV")
-snake=Animal("SURUNUR","ILAN","FISS")
-
-snake.pet_info()
-dog.speak()      
+#     def pet_info(self):
+#         print("My",self.pet,"has the name",self.name,"they make sound",self.sound)
     
 
+# dog =Animal("XIRT","IT","HAVHAV")
+# snake=Animal("SURUNUR","ILAN","FISS")
+
+# snake.pet_info()
+# dog.speak()      
+   
+# class Player():
+#     def __init__(self,name,score):
+#         self.name=name
+#         self.score=score
+#         self.team=None
+          
+#     def show_stats(self):
+#         print("Player name is",self.name)
+#         print("Player score for this year is",self.score)
+#         print("Team: ",self.team)
         
-        
+#     def select_team(self):
+#         team_selection =input("Which team he will be playing this year:?\n")
+#         self.team=team_selection
+ 
+
+# player1 =Player("Zeynal","90")
+# player2 =Player("John","80")
+
+# player2.select_team()
+# player1.select_team()
+
+# player1.show_stats()
+# player2.show_stats()
